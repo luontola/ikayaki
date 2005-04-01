b5894371b9aaf833df37b5c01e378597b7bf056a
Samuli Kaipiainen <samuli.kaipiainen@cs.helsinki.fi>
2005-04-01 13:40:42 +0000

    <No Comment Entered>

--- copy_resources.bat
+++ copy_resources.bat
@@ -1,2 +1,3 @@
-copy resources\*.* classes\resources\
-cls
+:: unnecessary since MainViewPanel 1.17 -- or, that might have broken this for someone else :)
+:: anyway, classes-dir is the wrong place for these (jbuilder even empties it on rebuild)
+xcopy resources classes\resources\
