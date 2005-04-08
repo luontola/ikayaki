88c7685895d8869c93fc78f918216f733c194ac5
Aki Korpua <aki.korpua@cs.helsinki.fi>
2005-04-08 20:03:43 +0000

    <No Comment Entered>

--- src/ikayaki/squid/SquidEmulator.java
+++ src/ikayaki/squid/SquidEmulator.java
@@ -23,46 +23,50 @@
 package ikayaki.squid;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Stack;
 
 /**
  * This class tries to emulate behavior of real squid-system. It starts 3 threads (handler,magnetometer,degausser),
  * opens COM-ports for them and adds SerialIO Listeners. Threads generates random data values or loaded values as
  * results and generates random error situations to see that program using real squid system does survive those. Uses
  * 2-3 COM ports. Usage SquidEmulator x z.. filename where x is 0 or 1 and indicates if Magnetometer and Demagnetizer
- * are on same COM port. z... values are COM ports. filename is name of log file we are using or it is existing log
+ * are on same COM port. z... values are COM ports (Handler,Magnetometer,Degausser). filename is name of log file we are using or it is existing log
  * file, which is used to generate same sequence used to verify that old and new program behaves same way.
  *
- * @author
+ * @author Aki Korpua
  */
 public class SquidEmulator {
-/*
+
+  /*
 Event A: On New IO Message - reads message and puts it in Buffer
 */
 
     /**
      * indicates if system have been started
      */
-    private boolean online;
+    private static boolean online;
 
     /**
      * log file we are using read or write
      */
-    private File logFile;
+    private static File logFile;
 
     /**
      * indicates have we loaded log file for using or are we writing it
      */
-    private boolean usingOldLog;
+    private static boolean usingOldLog;
 
     /**
      * value between 0 and 127 default 5. Settings in the 20-50 range are usually employed.
      */
-    private int acceleration;
+    private static int acceleration;
 
     /**
      * value between 0 and 127 default 10. Settings in the 20-50 range are usually employed.
      */
-    private int deceleration;
+    private static int deceleration;
 
     /**
      * value between 50 and 12 000. The decimal number issued is 10 times the actual pulse rate to the motor. Since the
@@ -71,72 +75,76 @@ Event A: On New IO Message - reads message and puts it in Buffer
      * one-half the sample rate rotation due to the pulley ratios. The sample handler is set up at the factory for half
      * stepping.
      */
-    private int velocity;
+    private static int velocity;
 
     /**
      * 5 end of move, previous G command complete, 7 hard limit stop, G motor is currently indexing
      */
-    private String handlerStatus;
+    private static String handlerStatus;
 
     /**
      * value between 1 and 16,777,215
      */
-    private int commandedDistance;
+    private static int commandedDistance;
 
     /**
      * value between 1 and 16,777,215
      */
-    private int currentPosition;
+    private static int currentPosition;
 
     /**
      * value between 1 and 16,777,215
      */
-    private int homePosition;
+    private static int homePosition;
 
     /**
      * angles are between 0 (0) and 2000 (360)
      */
-    private int commandedRotation;
+    private static int commandedRotation;
 
     /**
      * angles are between 0 (0) and 2000 (360)
      */
-    private int currentRotation;
+    private static int currentRotation;
 
     /**
      * (X, Y, Z) = (0,1,2) default axis Z
      */
-    private int degausserCoil;
+    private static int degausserCoil;
 
     /**
      * 0->3000 default amp 0
      */
-    private int degausserAmplitude;
+    private static int degausserAmplitude;
 
     /**
      * 1-9 seconds default delay 1 second
      */
-    private int degausserDelay;
+    private static int degausserDelay;
 
     /**
      * (3, 5, 7, 9) default 3
      */
-    private int degausserRamp;
+    private static int degausserRamp;
 
     /**
      * Z=Zero, T=Tracking, ?=Unknown
      */
-    private char degausserStatus;
+    private static char degausserStatus;
 
     /**
      * starts Threads which reads messages from selected COM port. Own listener for each. Offers write commads to port
      * too.
      */
-    private SerialIO[] serialIO;
+    private static SerialIO handlerPort;
+    private static SerialIO magnetometerPort;
+    private static SerialIO degausserPort;
 
-    private HandlerEmu handler = new HandlerEmu();
-    private MagnetometerEmu magnetometer = new MagnetometerEmu();
-    private DegausserEmu degausser = new DegausserEmu();
+    private static FileWriter logWriter = null;
+
+    private static HandlerEmu handler;
+    private static MagnetometerEmu magnetometer;
+    private static DegausserEmu degausser;
 
     /**
      * send message to SerialIO to be sented.
@@ -144,8 +152,21 @@ Event A: On New IO Message - reads message and puts it in Buffer
      * @param message any message reply we are sending back
      * @param port    port number to be sent
      */
-    public void writeMessage(String message, int port) {
-        return; // TODO
+    public static void writeMessage(String message, SerialIO port) {
+      //writes log if indicated to do so
+      try {
+        logWriter.write("SEND:" + message);
+      }
+      catch(IOException e) {
+        System.err.println("Error on writing log file");
+      }
+      try {
+        port.writeMessage(message);
+      }
+      catch (SerialIOException ex) {
+        System.err.println(ex);
+      }
+      return;
     }
 
     /**
@@ -153,36 +174,117 @@ Event A: On New IO Message - reads message and puts it in Buffer
      * actions (move, rotate, demag, measure) and send feedback to COM ports.
      */
     public static void main(String[] args) {
-        return; // TODO
+    try {
+        int samePort = Integer.parseInt(args[0]);
+        handlerPort = new SerialIO(new SerialParameters(args[1]));
+        magnetometerPort = new SerialIO(new SerialParameters(args[2]));
+        if(samePort == 0)
+          degausserPort = new SerialIO(new SerialParameters(args[3]));
+        else
+          degausserPort = magnetometerPort;
+        if(samePort == 0)
+          logFile = new File(args[4]);
+        else
+          logFile = new File(args[5]);
+        //Only writing, TODO:switch to reading option
+        logWriter = new FileWriter(logFile);
+      }
+      catch (SerialIOException e) {
+        System.out.println(e);
+      }
+      catch(Exception e) {
+        System.out.println("Usage \"java SquidEmulator x z... filename\"");
+      }
+
+      handler = new HandlerEmu();
+      magnetometer = new MagnetometerEmu();
+      degausser = new DegausserEmu();
+
+      try {
+        //wait for signal to quit 8)
+        System.in.read();
+      }
+      catch (IOException ex) {
+      }
+
+      return;
     }
 
-    /**
+
+  /**
      * Runs handler emulation process. Process incoming messages and sends data back. When message comes, process it
      * (wait if needed for a while), updates own status and sends result back.
      */
-    private class HandlerEmu extends Thread {
+    private static class HandlerEmu extends Thread implements SerialIOListener {
+
+
+      //All recieved commands
+      private Stack commandStack;
+
+      //remainder of last command (commands are separated with ',')
+      private String lastMessagePart;
+
+      public HandlerEmu() {
+        //Setlistener to handlePort
+        //handlerPort.set
+      }
+
         public void run() {
             // TODO
         }
+
+    public void serialIOEvent(SerialIOEvent event) {
+      int i;
+      String message = lastMessagePart + event.getMessage();
+      String[] commands = message.split(",");
+      for(i = 0;i<commands.length-1;i++) {
+        commandStack.add(commands[i]);
+      }
+      lastMessagePart = commands[i+1];
+    }
+  }
+
+  /**
+   * Runs magnetometer emulation process. Process incoming messages and sends data back. When message comes, process
+   * it (wait if needed for a while), updates own status and sends result back.
+   */
+  private static class MagnetometerEmu extends Thread implements SerialIOListener {
+    //All recieved commands
+    private Stack commandStack;
+
+    public void run() {
+      // TODO
     }
 
-    /**
-     * Runs magnetometer emulation process. Process incoming messages and sends data back. When message comes, process
-     * it (wait if needed for a while), updates own status and sends result back.
-     */
-    private class MagnetometerEmu extends Thread {
-        public void run() {
-            // TODO
-        }
+    public void serialIOEvent(SerialIOEvent event) {
+      String message = event.getMessage();
+      String[] commands = message.split("/r");
+      //we only accept first "part". And only if it starts with A,X,Y,Z
+      if(commands[0].charAt(0) == 'A' || commands[0].charAt(0) == 'X' || commands[0].charAt(0) == 'Y' || commands[0].charAt(0) == 'Z')
+        commandStack.add(commands[0]);
+    }
+  }
+
+  /**
+   * Runs degausser emulation process. Process incoming messages and sends data back. When message comes, process it
+   * (wait if needed for a while), updates own status and sends result back.
+   */
+  private static class DegausserEmu
+      extends Thread implements SerialIOListener {
+
+    //All recieved commands
+    private Stack commandStack;
+
+    public void run() {
+      // TODO
     }
 
-    /**
-     * Runs degausser emulation process. Process incoming messages and sends data back. When message comes, process it
-     * (wait if needed for a while), updates own status and sends result back.
-     */
-    private class DegausserEmu extends Thread {
-        public void run() {
-            // TODO
-        }
+    public void serialIOEvent(SerialIOEvent event) {
+      String message = event.getMessage();
+      String[] commands = message.split("/r");
+      //we only accept first "part". And only if it starts with D
+      if(commands[0].charAt(0) == 'D')
+        commandStack.add(commands[0]);
     }
+  }
 }
