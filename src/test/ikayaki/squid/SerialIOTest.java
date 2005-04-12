20bc3433c3ebb2aaed3b4b972a9a775931b48836
Aki Sysmäläinen <aki.sysmalainen@helsinki.fi>
2005-04-12 19:06:52 +0000

    no message

--- src/test/ikayaki/squid/SerialIOTest.java
+++ src/test/ikayaki/squid/SerialIOTest.java
@@ -1,11 +1,10 @@
 package test.ikayaki.squid;
 
-import junit.framework.*;
-import ikayaki.squid.SerialIOException;
-import ikayaki.squid.SerialParameters;
-import ikayaki.squid.SerialIO;
-import ikayaki.squid.SerialIOListener;
-import ikayaki.squid.SerialIOEvent;
+import ikayaki.squid.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 import java.util.concurrent.SynchronousQueue;
 
 /**
@@ -14,55 +13,52 @@ import java.util.concurrent.SynchronousQueue;
  * @author Aki Sysmäläinen
  */
 public class SerialIOTest
-    extends TestCase implements SerialIOListener {
+        extends TestCase implements SerialIOListener {
 
-  private SynchronousQueue queue;
+    private SynchronousQueue queue;
 
-  public static Test suite() {
-    return new TestSuite(SerialIOTest.class);
-  }
-
-  public static void main(String args[]) {
-    junit.textui.TestRunner.run(suite());
-  }
-
-  //requires that you have Virtual serial connection from COM2 to COM3
-  public void testSerialIO() {
-    SerialIO com2;
-    SerialIO com3;
-    try {
-      com2 = SerialIO.openPort(new SerialParameters("COM2"));
-      com3 = SerialIO.openPort(new SerialParameters("COM3"));
-      com3.addSerialIOListener(this);
-      assertTrue(com2 != null);
-      assertTrue(com3 != null);
-      String testString = "S124,5//r";
-      com2.writeMessage(testString);
-      assertTrue(testString.equals( (String) queue.poll()));
+    public static Test suite() {
+        return new TestSuite(SerialIOTest.class);
     }
-    catch (SerialIOException e) {
-      fail(e.toString());
+
+    public static void main(String args[]) {
+        junit.textui.TestRunner.run(suite());
     }
-  }
 
-  public void testWriteMessage() {
-
-  }
-
-  public void testGetLastMessage() {
-
-  }
-
-  public void serialIOEvent(SerialIOEvent event) {
-    try {
-      queue.put(event.getMessage());
+    //requires that you have Virtual serial connection from COM2 to COM3
+    public void testSerialIO() {
+        SerialIO writePort;
+        SerialIO readPort;
+        try {
+            writePort = SerialIO.openPort(new SerialParameters("COM4"));
+            readPort = SerialIO.openPort(new SerialParameters("COM7"));
+            readPort.addSerialIOListener(this);
+            assertTrue(writePort != null);
+            assertTrue(readPort != null);
+            String testString = "S124,5//r";
+            writePort.writeMessage(testString);
+            assertTrue(testString.equals((String) queue.poll()));
+        } catch (SerialIOException e) {
+            fail(e.toString());
+        }
     }
-    catch (InterruptedException e) {
-      System.err.println("Interrupted Degausser message event");
+
+    public void testWriteMessage() {
+
     }
-    catch (NullPointerException e) {
-      System.err.println("Null from SerialEvent in Degausser");
+
+    public void testGetLastMessage() {
+
     }
-  }
+
+    public void serialIOEvent(SerialIOEvent event) {
+        try {
+            queue.put(event.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("Interrupted Degausser message event");
+        } catch (NullPointerException e) {
+            System.err.println("Null from SerialEvent in Degausser");
+        }
+    }
 
 }
