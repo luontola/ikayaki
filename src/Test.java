25c5193e35f699581b7b3690ae82a46c43d48c57
Esko Luontola <esko.luontola@cs.helsinki.fi>
2005-04-17 20:56:32 +0000

    no message

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,18 @@
 
+import java.util.Arrays;
+import java.util.Set;
+
 /**
  * Testitiedosto
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("\u00b0");
+        Set<Object> keyset = System.getProperties().keySet();
+        Object[] keys = keyset.toArray();
+        Arrays.sort(keys);
+        for (Object key : keys) {
+            System.out.println(key + " = " + System.getProperty(key.toString()));
+        }
+//        System.out.println("\u00b0");
     }
 }
