cfe770344465318f588172c8ea37cc248cae8759
Samuli Kaipiainen <samuli.kaipiainen@cs.helsinki.fi>
2005-05-04 15:06:50 +0000

    murinakeksimurahdus

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Set;
 
 /**
- * Testitiedosto
+ * Testitiedosto... Arrrrrrr-nnnnnnnnn-splaaaaats-bluppppps!
  */
 public class Test {
     public static void main(String[] args) {
